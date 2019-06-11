f5d682eed97cc24740dc3281a1fb7cbfccd9cb2b
Hossam777 <31036061+Hossam777@users.noreply.github.com>
2019-06-11 15:25:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,60 @@ const express = require('express');
 
 const app = express();
 
-app.get('/numbers', (req, res, next) => {
-    
-    /*var nums = req.body.nums;
+function switchWith (numbers, diffrence, outputSet) {
+    var i, j;
+    for(i = 0; i < outputSet.length; i++){
+        for(j = 0; j < numbers.length; j++){
+            if(outputSet[i] < parseInt(numbers[j]) && parseInt(numbers[j]) - outputSet[i] <= diffrence){
+                diffrence -= parseInt(numbers[j]) - outputSet[i];
+                outputSet[i] = parseInt(numbers[j]);
+            }
+            if(diffrence == 0){
+                return outputSet;
+            }
+        }
+    }
+    return null;
+}
 
-    var length = req.body.length;
+app.get('/numbers', (req, res, next) => {
+    var len = req.query.length, 
+        numbersSet = req.query.numbersset, 
+        avg = req.query.average;
+     
+    if(!Array.isArray(numbersSet)){
+        res.status(400).json({
+            numbers : "bad request : array is invalid input"
+        });
+    }
+    len = parseInt(len);
+    if(isNaN(avg)){
+        res.status(400).json({
+            numbers : "bad request : averag is invalid input"
+        });
+    }
+    var i = 0, sum = 0, target = parseInt(len * avg);
+    var output = new Array();
     
-    var average = req.body.average;
-*/
+    while (output.length < len){
+	    if(sum + parseInt(numbersSet[i]) <= target){
+	        output.push(parseInt(numbersSet[i]));
+	        sum += parseInt(numbersSet[i++]);
+	    }
+        if(i == numbersSet.length)
+	        i = 0;
+    }
+    
+    if(sum != (len * avg)){
+	    output = switchWith(numbersSet, target - sum, output);
+    }
+    if(output == null){ 
+        res.status(200).json({
+            numbers : "can't decide numbers"
+        });
+    }
     res.status(200).json({
-        numbers : 'test'
+        numbers : output
     });
 });
 
